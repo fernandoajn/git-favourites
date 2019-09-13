da75d82bfff47425f19727fb240fc4561436af90
Fernando Augusto <fernandoajneto@gmail.com>
2019-09-13 20:30:04 -0300

    Listing users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,19 @@ import {Keyboard} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
-import {Container, Form, Input, SubmitButton} from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default class Main extends Component {
   state = {
@@ -28,6 +40,8 @@ export default class Main extends Component {
       newUser: '',
     });
 
+    console.log(data);
+
     Keyboard.dismiss();
   };
 
@@ -40,7 +54,7 @@ export default class Main extends Component {
           <Input
             autoCorrect={false}
             autoCapitalize="none"
-            placeholder="Adicionar usuário"
+            placeholder="Add a new user"
             value={newUser}
             onChangeText={text => this.setState({newUser: text})}
             returnKeyType="send"
@@ -50,6 +64,22 @@ export default class Main extends Component {
             <Icon name="add" size={20} color="#FFF" />
           </SubmitButton>
         </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({item}) => (
+            <User>
+              <Avatar source={{uri: item.avatar}} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     );
   }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -33,5 +33,56 @@ export const SubmitButton = styled(RectButton)`
   padding: 0 12px;
 `;
 
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const User = styled.View`
+  align-items: center;
+  margin: 0 20px 30px;
+`;
+
+export const Avatar = styled.Image`
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+  background: #eee;
+`;
+
+export const Name = styled.Text`
+  font-size: 14px;
+  color: #333;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 4px;
+`;
+
+export const Bio = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  font-size: 13px;
+  color: #999;
+  line-height: 18px;
+  margin-top: 5px;
+  text-align: center;
+`;
+export const ProfileButton = styled(RectButton)`
+  align-self: stretch;
+  margin-top: 10px;
+  border-radius: 4px;
+  background: #ff9900;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+`;
+export const ProfileButtonText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #fff;
+`;
+
 // Nesting doesn't work on React Native
 // Can't create global styles
